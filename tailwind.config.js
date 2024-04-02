a548eb09c4846c36e36ae3a103ba2a2b27172315
Your Name <you@example.com>
2024-04-02 12:39:23 +0530

    Random quote

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/**@type{import('tailwindcss').Config} */
+
+
+export default{
+    mode: 'jit',
+    content:["{js,cjs,mjs,ts}"],
+    theme:{
+       
+        extend:{
+            screens:{},
+            
+        },
+    },
+    plugins:[],
+};
